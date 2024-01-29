0d4ac716bb8ba4eed26d01e425b475781d269b1f
unknown <apatte@gamingcampus.local>
2024-01-29 15:02:08 +0100

    V1

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        if (OperatingSystem.IsWindows())
+        {
+          Console.WindowWidth = 40;
+          Console.WindowHeight = 20;
+        }
+
+
+        char[,] carte =
+        {
+            {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+            {'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '#'},
+            {'#', '.', '.', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#'},
+            {'#', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '.', '.', '.', '#', '#'},
+            {'#', '.', '.', '#', '.', '#', '#', '#', '#', '.', '.', '.', '.', '.', '#', '.', '.', '.', '#', '#'},
+            {'#', '.', '.', '#', '.', '.', '.', '.', '#', '#', '#', '#', '#', '.', '#', '.', '#', '.', '.', '#'},
+            {'#', '.', '.', '#', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#', '.', '#', '.', '.', '#'},
+            {'#', '.', '.', '#', '#', '.', '#', '.', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '.', '#'},
+            {'#', '.', '.', '.', '#', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '.', '#'},
+            {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
+        };
+
+
+        /* pos du départ du joueur */
+        int posX = 1;
+        int posY = 1;
+
+        /* Afficher la map au start */
+        AfficherCarte(carte, posX, posY);
+
+        static void AfficherCarte(char[,] carte, int posX, int posY)
+        {
+            for (int i = 0; i < carte.GetLength(0); i++)
+            {
+                for (int j = 0; j < carte.GetLength(1); j++)
+                {
+                    if (i == posY && j == posX)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("P ");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write(carte[i, j] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        ConsoleKeyInfo keyInfo;
+
+        do
+        {
+            keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.Z:
+                    if (posY > 1 && carte[posY - 1, posX] == '.')
+                    {
+                        carte[posY, posX] = '.';
+                        posY--;
+                    }
+                    break;
+                case ConsoleKey.S:
+                    if (posY < carte.GetLength(0) - 2 && carte[posY + 1, posX] == '.')
+                    {
+                        carte[posY, posX] = '.';
+                        posY++;
+                    }
+                    break;
+                case ConsoleKey.Q:
+                    if (posX > 1 && carte[posY, posX - 1] == '.')
+                    {
+                        carte[posY, posX] = '.';
+                        posX--;
+                    }
+                    break;
+                case ConsoleKey.D:
+                    if (posX < carte.GetLength(1) - 2 && carte[posY, posX + 1] == '.')
+                    {
+                        carte[posY, posX] = '.';
+                        posX++;
+                    }
+                    break;
+            }
+
+            Console.Clear();
+            AfficherCarte(carte, posX, posY);
+
+        } while (keyInfo.Key != ConsoleKey.Escape);
+
+        Console.ReadLine();
+        
+    }
+}
