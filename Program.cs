d239eb428c53951e71ba7618014e0f58c1430b4e
LiLPray <gandre@gaming.tech>
2024-01-31 11:34:15 +0100

    Hautes Herbes + Base logique combat

--- Program.cs
+++ Program.cs
@@ -49,8 +49,8 @@ class Program
             {'#', '.', '.', '.', '#', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#', '.', '#', '.', '.', '#'},
             {'#', '.', '.', '.', '#', '.', '#', '#', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
             {'#', '.', '.', '.', '#', '.', '.', '#', '.', '.', '#', '.', '.', '.', '.', '.', '#', '.', '.', '#'},
-            {'#', '.', '.', '.', '#', '.', '.', '.', '.', '.', '#', '#', '#', '#', '.', '.', '#', '.', '.', '#'},
-            {'#', '.', '.', '.', '#', '.', '#', '.', '.', '.', '.', '#', '.', '.', '.', '.', '#', '.', '#', '#'},
+            {'#', 'H', 'H', 'H', '#', '.', '.', '.', '.', '.', '#', '#', '#', '#', '.', '.', '#', '.', '.', '#'},
+            {'#', 'H', 'H', 'H', '#', '.', '#', '.', '.', '.', '.', '#', '.', '.', '.', '.', '#', '.', '#', '#'},
             {'#', '.', '.', '.', '#', '.', '#', '.', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#'},
             {'#', '.', '.', '.', '.', '.', '#', '.', '.', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#'},
             {'#', '.', '.', '.', '#', '.', '#', '#', '#', '#', '.', '.', '.', '#', '.', '.', '.', '.', '.', '#'},
@@ -59,7 +59,6 @@ class Program
             {'#', '.', '.', '.', '#', '.', '.', '.', '.', '#', '.', '.', '.', '.', '#', '.', '.', '.', '.', '#'},
             {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
         };
-
         return carte;
     }
 
@@ -115,13 +114,13 @@ class Program
     {
         switch (keyInfo.Key)
         {
-            case ConsoleKey.W:
+            case ConsoleKey.Z:
                 MovePlayerIfValid(-1, 0, carte);
                 break;
             case ConsoleKey.S:
                 MovePlayerIfValid(1, 0, carte);
                 break;
-            case ConsoleKey.A:
+            case ConsoleKey.Q:
                 MovePlayerIfValid(0, -1, carte);
                 break;
             case ConsoleKey.D:
@@ -137,7 +136,12 @@ class Program
 
         if (IsValidMove(newPosY, newPosX, carte))
         {
-            carte[posY, posX] = '.';
+            // Vérifier spécifiquement si le joueur est sur une case avec des hautes herbes
+            if (carte[newPosY, newPosX] == 'H')
+            {
+                LancerCombatSiRencontrePokemon(carte);
+            }
+
             posY = newPosY;
             posX = newPosX;
         }
@@ -145,7 +149,7 @@ class Program
 
     static bool IsValidMove(int y, int x, char[,] carte)
     {
-        return y >= 0 && y < carte.GetLength(0) && x >= 0 && x < carte.GetLength(1) && carte[y, x] == '.';
+        return y >= 0 && y < carte.GetLength(0) && x >= 0 && x < carte.GetLength(1) && carte[y, x] == '.' || carte[y, x] == 'H';
     }
 
     static void ProcessDifficultyChoice()
@@ -202,16 +206,34 @@ class Program
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.Write("P ");
-                    Console.ResetColor();
                 }
                 else
                 {
+                    // Couleur marron pour les murs
+                    if (carte[i, j] == '#')
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    }
+                    // Couleur verte pour les hautes herbes
+                    else if (carte[i, j] == 'H')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        Console.ResetColor();
+                    }
+
                     Console.Write(carte[i, j] + " ");
                 }
             }
             Console.WriteLine();
         }
+
+        // Assure-toi de réinitialiser la couleur après avoir affiché la carte
+        Console.ResetColor();
     }
+
 
     static void main_menu()
     {
@@ -233,4 +255,130 @@ class Program
 
         Thread.Sleep(durationMilliseconds);
     }
+    static void LancerCombatSiRencontrePokemon(char[,] carte)
+    {
+        Random random = new Random();
+
+        // Vérifier si le joueur est sur une case avec un Pokémon
+        if (carte[posY, posX] == 'H' && random.Next(1, 10) == 1)
+        {
+            // Rencontre un Pokémon
+            Console.WriteLine("Vous avez rencontré un Pokémon sauvage !");
+
+            // Sélectionner un Pokémon au hasard depuis la bibliothèque
+            List<Pokemon> listePokemon = BibliothequePokemon.GetListePokemon();
+            Pokemon pokemonRencontre = listePokemon[random.Next(listePokemon.Count)];
+
+            Console.WriteLine($"Un {pokemonRencontre.Nom} sauvage apparaît !");
+            Console.WriteLine($"Points de vie du {pokemonRencontre.Nom}: {pokemonRencontre.PointsDeVie}");
+
+            // Combat
+            while (pokemonRencontre.PointsDeVie > 0)
+            {
+                // Tour du joueur
+                Console.WriteLine("\nC'est à votre tour :");
+                AfficherAttaquesJoueur(pokemonRencontre);
+                int choixAttaque = DemanderChoixAttaque(pokemonRencontre.Attaques.Count);
+
+                // Appliquer les dégâts à Pokémon sauvage
+                int degatsInfliges = pokemonRencontre.Attaques[choixAttaque - 1].Puissance;
+                pokemonRencontre.PointsDeVie -= degatsInfliges;
+
+                Console.WriteLine($"Vous avez infligé {degatsInfliges} points de dégâts.");
+                Console.WriteLine($"Points de vie restants du {pokemonRencontre.Nom}: {pokemonRencontre.PointsDeVie}");
+
+                if (pokemonRencontre.PointsDeVie <= 0)
+                {
+                    Console.WriteLine($"Le {pokemonRencontre.Nom} sauvage a été vaincu !");
+                    break;
+                }
+
+                // Tour du Pokémon sauvage
+                Console.WriteLine($"\nC'est au tour du {pokemonRencontre.Nom} sauvage :");
+                int choixAttaquePokemon = random.Next(pokemonRencontre.Attaques.Count);
+
+                // Appliquer les dégâts au joueur
+                int degatsInfligesPokemon = pokemonRencontre.Attaques[choixAttaquePokemon].Puissance;
+                // En supposant que le joueur a 100 points de vie initiaux
+                Console.WriteLine($"{pokemonRencontre.Nom} a infligé {degatsInfligesPokemon} points de dégâts.");
+                Console.WriteLine($"Points de vie restants du joueur: {100 - degatsInfligesPokemon}");
+
+                if (100 - degatsInfligesPokemon <= 0)
+                {
+                    Console.WriteLine($"Vous avez été vaincu par le {pokemonRencontre.Nom} sauvage !");
+                    break;
+                }
+            }
+
+            // Réinitialiser la case de la carte à '.' seulement si le Pokémon n'a pas été vaincu
+            if (pokemonRencontre.PointsDeVie > 0)
+            {
+                carte[posY, posX] = '.';
+            }
+        }
+    }
+
+    static void AfficherAttaquesJoueur(Pokemon pokemon)
+    {
+        Console.WriteLine("Choisissez une attaque :");
+        for (int i = 0; i < pokemon.Attaques.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {pokemon.Attaques[i].Nom} (Puissance : {pokemon.Attaques[i].Puissance})");
+        }
+    }
+
+    static int DemanderChoixAttaque(int nombreAttaques)
+    {
+        int choix = 0;
+        do
+        {
+            Console.Write("Votre choix : ");
+        } while (!int.TryParse(Console.ReadLine(), out choix) || choix < 1 || choix > nombreAttaques);
+
+        return choix;
+    }
+
 }
+
+
+public class Pokemon
+{
+    public string Nom { get; set; }
+    public int PointsDeVie { get; set; }
+    public List<Attaque> Attaques { get; set; }
+
+    public Pokemon(string nom, int pointsDeVie, List<Attaque> attaques)
+    {
+        Nom = nom;
+        PointsDeVie = pointsDeVie;
+        Attaques = attaques;
+    }
+}
+
+public class Attaque
+{
+    public string Nom { get; set; }
+    public int Puissance { get; set; }
+
+    public Attaque(string nom, int puissance)
+    {
+        Nom = nom;
+        Puissance = puissance;
+    }
+}
+
+public class BibliothequePokemon
+{
+    public static List<Pokemon> GetListePokemon()
+    {
+        List<Pokemon> listePokemon = new List<Pokemon>();
+
+        // Ajoutez vos Pokémon ici
+        listePokemon.Add(new Pokemon("Pikachu", 100, new List<Attaque> { new Attaque("Éclair", 20), new Attaque("Queue de fer", 15) }));
+        listePokemon.Add(new Pokemon("Bulbizarre", 120, new List<Attaque> { new Attaque("Fouet lianes", 18), new Attaque("Vampigraine", 12) }));
+        // ... Ajoutez d'autres Pokémon de la même manière
+
+        return listePokemon;
+    }
+}
+
