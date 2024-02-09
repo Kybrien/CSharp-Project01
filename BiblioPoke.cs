49507685c048c6ceb8dd1029f49add739f2ad7a1
Hroycode <hroy@gaming.tech>
2024-02-09 04:35:20 +0100

    combat boss

--- BiblioPoke.cs
+++ BiblioPoke.cs
@@ -2,7 +2,52 @@
 {
     public static class BibliothequePokemon
     {
-        public static List<Pokemon> GetListePokemon()
+        public static List<Pokemon> GetListeStarter()
+        {
+            List<Pokemon> listePokemon = new List<Pokemon>();
+
+            listePokemon.Add(new Pokemon("Venusaur", "Grass/Poison", 80, 82, 83, 100, 100, 80, new List<Capacite> {new Capacite("Solar Beam", "Grass", 120, 100, "Special"),new Capacite("Sludge Bomb", "Poison", 90, 100, "Special"),new Capacite("Sleep Powder", "Grass", 0, 75, "Status"),new Capacite("Earthquake", "Ground", 100, 100, "Physical")}));
+
+            listePokemon.Add(new Pokemon("Blastoise", "Water", 79, 83, 100, 85, 105, 78, new List<Capacite> {new Capacite("Hydro Pump", "Water", 110, 80, "Special"),new Capacite("Ice Beam", "Ice", 90, 100, "Special"),new Capacite("Flash Cannon", "Steel", 80, 100, "Special"),new Capacite("Protect", "Normal", 0, 100, "Status")}));
+
+            listePokemon.Add(new Pokemon("Charizard", "Fire/Flying", 78, 84, 78, 109, 85, 100, new List<Capacite> {new Capacite("Flamethrower", "Fire", 90, 100, "Special"),new Capacite("Fly", "Flying", 90, 95, "Physical"),new Capacite("Dragon Claw", "Dragon", 80, 100, "Physical"),new Capacite("Solar Beam", "Grass", 120, 100, "Special")}));
+
+
+
+            return listePokemon;
+        }
+        public static List<Pokemon> GetListeMewtwo()
+        {
+            List<Pokemon> listePokemon = new List<Pokemon>();
+            listePokemon.Add(new Pokemon("Mewtwo", "Psychic", 106, 110, 90, 154, 90, 130, new List<Capacite> { new Capacite("Psychic", "Psychic", 90, 100, "Special"), new Capacite("Aura Sphere", "Fighting", 80, 100, "Special"), new Capacite("Shadow Ball", "Ghost", 80, 100, "Special"), new Capacite("Recover", "Normal", 0, 100, "Status") }));
+
+
+            return listePokemon;
+        }
+        public static List<Pokemon> GetListePokeZone1()
+        {
+            List<Pokemon> listePokemon = new List<Pokemon>();
+
+            listePokemon.Add(new Pokemon("Bulbasaur", "Grass/Poison", 45, 49, 49, 65, 65, 45, new List<Capacite> { new Capacite("Tackle", "Normal", 40, 100, "Physical"), new Capacite("Growl", "Normal", 0, 100, "Status") }));
+            listePokemon.Add(new Pokemon("Charmander", "Fire", 39, 52, 43, 60, 50, 65, new List<Capacite> { new Capacite("Scratch", "Normal", 40, 100, "Physical"), new Capacite("Earthquake", "Ground", 100, 100, "Physical") }));
+            listePokemon.Add(new Pokemon("Charizard", "Fire/Flying", 78, 84, 78, 109, 85, 100, new List<Capacite> { new Capacite("Fire Spin", "Fire", 35, 85, "Special"), new Capacite("Air Slash", "Flying", 75, 95, "Special") }));
+            listePokemon.Add(new Pokemon("Squirtle", "Water", 44, 48, 65, 50, 64, 43, new List<Capacite> { new Capacite("Tackle", "Normal", 40, 100, "Physical"), new Capacite("Bubble", "Water", 40, 100, "Special") }));
+            listePokemon.Add(new Pokemon("Caterpie", "Bug", 45, 30, 35, 20, 20, 45, new List<Capacite> { new Capacite("Tackle", "Normal", 40, 100, "Physical"), new Capacite("String Shot", "Bug", 0, 95, "Status") }));
+            listePokemon.Add(new Pokemon("Metapod", "Bug", 50, 20, 55, 25, 25, 30, new List<Capacite> { new Capacite("Harden", "Normal", 0, 100, "Status"), new Capacite("Tackle", "Normal", 40, 100, "Physical") }));
+            listePokemon.Add(new Pokemon("Butterfree", "Bug/Flying", 60, 45, 50, 90, 80, 70, new List<Capacite> { new Capacite("Gust", "Flying", 40, 100, "Special"), new Capacite("Confusion", "Psychic", 50, 100, "Special") }));
+            listePokemon.Add(new Pokemon("Weedle", "Bug/Poison", 40, 35, 30, 20, 20, 50, new List<Capacite> { new Capacite("Poison Sting", "Poison", 15, 100, "Physical"), new Capacite("String Shot", "Bug", 0, 95, "Status") }));
+            listePokemon.Add(new Pokemon("Kakuna", "Bug/Poison", 45, 25, 50, 25, 25, 35, new List<Capacite> { new Capacite("Harden", "Normal", 0, 100, "Status"), new Capacite("Poison Sting", "Poison", 15, 100, "Physical") }));
+            listePokemon.Add(new Pokemon("Pidgey", "Normal/Flying", 40, 45, 40, 35, 35, 56, new List<Capacite> { new Capacite("Gust", "Flying", 40, 100, "Special"), new Capacite("Quick Attack", "Normal", 40, 100, "Physical") }));
+            listePokemon.Add(new Pokemon("Pidgeotto", "Normal/Flying", 63, 60, 55, 50, 50, 71, new List<Capacite> { new Capacite("Wing Attack", "Flying", 60, 100, "Physical"), new Capacite("Twister", "Dragon", 40, 100, "Special") }));
+            listePokemon.Add(new Pokemon("Rattata", "Normal", 30, 56, 35, 25, 35, 72, new List<Capacite> { new Capacite("Quick Attack", "Normal", 40, 100, "Physical"), new Capacite("Hyper Fang", "Normal", 80, 90, "Physical") }));
+            listePokemon.Add(new Pokemon("Spearow", "Normal/Flying", 40, 60, 30, 31, 31, 70, new List<Capacite> { new Capacite("Peck", "Flying", 35, 100, "Physical"), new Capacite("Fury Attack", "Normal", 15, 85, "Physical") }));
+            listePokemon.Add(new Pokemon("Ekans", "Poison", 35, 60, 44, 40, 54, 55, new List<Capacite> { new Capacite("Poison Sting", "Poison", 15, 100, "Physical"), new Capacite("Bite", "Dark", 60, 100, "Physical") }));
+            listePokemon.Add(new Pokemon("Pikachu", "Electric", 35, 55, 40, 50, 50, 90, new List<Capacite> { new Capacite("Thunder Shock", "Electric", 40, 100, "Special"), new Capacite("Quick Attack", "Normal", 40, 100, "Physical") }));
+
+
+            return listePokemon;
+        }
+        public static List<Pokemon> GetListePokeZone2()
         {
             List<Pokemon> listePokemon = new List<Pokemon>();
 
@@ -34,6 +79,41 @@
             listePokemon.Add(new Pokemon("Ekans", "Poison", 35, 60, 44, 40, 54, 55, new List<Capacite> { new Capacite("Poison Sting", "Poison", 15, 100, "Physical"), new Capacite("Bite", "Dark", 60, 100, "Physical") }));
             listePokemon.Add(new Pokemon("Arbok", "Poison", 60, 85, 69, 65, 79, 80, new List<Capacite> { new Capacite("Acid", "Poison", 40, 100, "Special"), new Capacite("Wrap", "Normal", 15, 90, "Physical") }));
             listePokemon.Add(new Pokemon("Pikachu", "Electric", 35, 55, 40, 50, 50, 90, new List<Capacite> { new Capacite("Thunder Shock", "Electric", 40, 100, "Special"), new Capacite("Quick Attack", "Normal", 40, 100, "Physical") }));
+
+
+            return listePokemon;
+        }
+        public static List<Pokemon> GetListePokeZone3()
+        {
+            List<Pokemon> listePokemon = new List<Pokemon>();
+
+            listePokemon.Add(new Pokemon("Venusaur", "Grass/Poison", 80, 82, 83, 100, 100, 80, new List<Capacite> { new Capacite("Solar Beam", "Grass", 120, 100, "Special"), new Capacite("Sludge Bomb", "Poison", 90, 100, "Special"), new Capacite("Sleep Powder", "Grass", 0, 75, "Status"), new Capacite("Earthquake", "Ground", 100, 100, "Physical") }));
+
+            listePokemon.Add(new Pokemon("Blastoise", "Water", 79, 83, 100, 85, 105, 78, new List<Capacite> { new Capacite("Hydro Pump", "Water", 110, 80, "Special"), new Capacite("Ice Beam", "Ice", 90, 100, "Special"), new Capacite("Flash Cannon", "Steel", 80, 100, "Special"), new Capacite("Protect", "Normal", 0, 100, "Status") }));
+
+            listePokemon.Add(new Pokemon("Charizard", "Fire/Flying", 78, 84, 78, 109, 85, 100, new List<Capacite> { new Capacite("Flamethrower", "Fire", 90, 100, "Special"), new Capacite("Fly", "Flying", 90, 95, "Physical"), new Capacite("Dragon Claw", "Dragon", 80, 100, "Physical"), new Capacite("Solar Beam", "Grass", 120, 100, "Special") }));
+            listePokemon.Add(new Pokemon("Salamence", "Dragon/Flying", 95, 135, 80, 110, 80, 100, new List<Capacite> { new Capacite("Dragon Claw", "Dragon", 80, 100, "Physical"), new Capacite("Fly", "Flying", 90, 95, "Physical"), new Capacite("Fire Fang", "Fire", 65, 95, "Physical"), new Capacite("Dragon Dance", "Dragon", 0, 100, "Status") }));
+            listePokemon.Add(new Pokemon("Garchomp", "Dragon/Ground", 108, 130, 95, 80, 85, 102, new List<Capacite> { new Capacite("Dragon Claw", "Dragon", 80, 100, "Physical"), new Capacite("Earthquake", "Ground", 100, 100, "Physical"), new Capacite("Fire Fang", "Fire", 65, 95, "Physical"), new Capacite("Swords Dance", "Normal", 0, 100, "Status") }));
+            listePokemon.Add(new Pokemon("Alakazam", "Psychic", 55, 50, 45, 135, 95, 120, new List<Capacite> { new Capacite("Psychic", "Psychic", 90, 100, "Special"), new Capacite("Focus Blast", "Fighting", 120, 70, "Special"), new Capacite("Shadow Ball", "Ghost", 80, 100, "Special"), new Capacite("Calm Mind", "Psychic", 0, 100, "Status") }));
+            listePokemon.Add(new Pokemon("Lucario", "Fighting/Steel", 70, 110, 70, 115, 70, 90, new List<Capacite> { new Capacite("Aura Sphere", "Fighting", 80, 100, "Special"), new Capacite("Flash Cannon", "Steel", 80, 100, "Special"), new Capacite("Dark Pulse", "Dark", 80, 100, "Special"), new Capacite("Swords Dance", "Normal", 0, 100, "Status") }));
+            listePokemon.Add(new Pokemon("Gyarados", "Water/Flying", 95, 125, 79, 60, 100, 81, new List<Capacite> { new Capacite("Hydro Pump", "Water", 110, 80, "Special"), new Capacite("Hurricane", "Flying", 110, 70, "Special"), new Capacite("Ice Fang", "Ice", 65, 95, "Physical"), new Capacite("Dragon Dance", "Dragon", 0, 100, "Status") }));
+            listePokemon.Add(new Pokemon("Dragonite", "Dragon/Flying", 91, 134, 95, 100, 100, 80, new List<Capacite> { new Capacite("Outrage", "Dragon", 120, 100, "Physical"), new Capacite("Fire Punch", "Fire", 75, 100, "Physical"), new Capacite("Thunder Punch", "Electric", 75, 100, "Physical"), new Capacite("Roost", "Flying", 0, 100, "Status") }));
+            listePokemon.Add(new Pokemon("Tyranitar", "Rock/Dark", 100, 134, 110, 95, 100, 61, new List<Capacite> { new Capacite("Stone Edge", "Rock", 100, 80, "Physical"), new Capacite("Crunch", "Dark", 80, 100, "Physical"), new Capacite("Earthquake", "Ground", 100, 100, "Physical"), new Capacite("Hyper Beam", "Normal", 150, 90, "Special") }));
+            listePokemon.Add(new Pokemon("Metagross", "Steel/Psychic", 80, 135, 130, 95, 90, 70, new List<Capacite> { new Capacite("Meteor Mash", "Steel", 90, 90, "Physical"), new Capacite("Psychic", "Psychic", 90, 100, "Special"), new Capacite("Hammer Arm", "Fighting", 100, 90, "Physical"), new Capacite("Bullet Punch", "Steel", 40, 100, "Physical") }));
+
+            listePokemon.Add(new Pokemon("Zapdos", "Electric/Flying", 90, 90, 85, 125, 90, 100, new List<Capacite> { new Capacite("Thunderbolt", "Electric", 90, 100, "Special"), new Capacite("Drill Peck", "Flying", 80, 100, "Physical"), new Capacite("Heat Wave", "Fire", 95, 90, "Special"), new Capacite("Roost", "Flying", 0, 100, "Status") }));
+
+
+            listePokemon.Add(new Pokemon("Machamp", "Fighting", 90, 130, 80, 65, 85, 55, new List<Capacite> { new Capacite("Dynamic Punch", "Fighting", 100, 50, "Physical"), new Capacite("Knock Off", "Dark", 65, 100, "Physical"), new Capacite("Stone Edge", "Rock", 100, 80, "Physical"), new Capacite("Bulk Up", "Fighting", 0, 100, "Status") }));
+
+
+            listePokemon.Add(new Pokemon("Snorlax", "Normal", 160, 110, 65, 65, 110, 30, new List<Capacite> { new Capacite("Body Slam", "Normal", 85, 100, "Physical"), new Capacite("Crunch", "Dark", 80, 100, "Physical"), new Capacite("Earthquake", "Ground", 100, 100, "Physical"), new Capacite("Rest", "Psychic", 0, 100, "Status") }));
+
+
+            listePokemon.Add(new Pokemon("Arcanine", "Fire", 90, 110, 80, 100, 80, 95, new List<Capacite> { new Capacite("Flare Blitz", "Fire", 120, 100, "Physical"), new Capacite("Extreme Speed", "Normal", 80, 100, "Physical"), new Capacite("Wild Charge", "Electric", 90, 100, "Physical"), new Capacite("Morning Sun", "Normal", 0, 100, "Status") }));
+
+            listePokemon.Add(new Pokemon("Gardevoir", "Psychic/Fairy", 68, 65, 65, 125, 115, 80, new List<Capacite> { new Capacite("Psychic", "Psychic", 90, 100, "Special"), new Capacite("Moonblast", "Fairy", 95, 100, "Special"), new Capacite("Shadow Ball", "Ghost", 80, 100, "Special"), new Capacite("Calm Mind", "Psychic", 0, 100, "Status") }));
+
 
 
             return listePokemon;
--- Combat.cs
+++ Combat.cs
@@ -11,7 +11,14 @@ namespace CombatLoader
     {
         public static bool fight_end { get; set; } = false;
         public static Random random2 = new Random();
-        public static List<Pokemon> listePokemon = BibliothequePokemon.GetListePokemon();
+        public static List<Pokemon> listePokemon = BibliothequePokemon.GetListeStarter();
+        public static List<Pokemon> listePokeZone1 = BibliothequePokemon.GetListePokeZone1();
+        public static List<Pokemon> listePokeZone2 = BibliothequePokemon.GetListePokeZone2();
+        public static List<Pokemon> listePokeZone3 = BibliothequePokemon.GetListePokeZone3();
+        public static List<Pokemon> listeBoss = BibliothequePokemon.GetListeMewtwo();
+
+
+
         public static Pokemon StartPoke = listePokemon[random2.Next(listePokemon.Count)];
         private static bool gameover = false;
 
@@ -43,7 +50,8 @@ namespace CombatLoader
                     }
                     // ---------------------------------Rencontre un Pokémon---------------------------------
                     // Sélectionner un Pokémon au hasard depuis la bibliothèque
-                    Pokemon pokemonRencontre = listePokemon[random.Next(listePokemon.Count)];
+
+                    Pokemon pokemonRencontre = listePokeZone1[random.Next(listePokeZone1.Count)];
                     int pvMaxRencontre = pokemonRencontre.PointsDeVie;
                     int pvMaxJoueur = pokemonJoueur.PointsDeVie;
 
@@ -133,7 +141,7 @@ namespace CombatLoader
                                 Random catchPokemon = new Random();
                                 Thread.Sleep(500);
 
-                                if (catchPokemon.Next(1, 10) < 7)
+                                if (catchPokemon.Next(1, 10) < 6)
                                 {
                                     Console.WriteLine($"Vous avez capturé {pokemonRencontre.Nom} !");
                                     PokeTeam.AjouterPokemon(pokemonRencontre);
@@ -205,7 +213,145 @@ namespace CombatLoader
             return choix;
         }
 
+        public static void LancerCombatBoss(char[,] carte, int playerPosX, int playerPosY)
+        {
+            Random random = new Random();
+            Pokemon pokemonJoueur = StartPoke;
 
+
+                    // ---------------------------------Rencontre Boss---------------------------------
+                    // Sélectionner un Pokémon au hasard depuis la bibliothèque
+                    Pokemon pokemonRencontre = listePokemon[random.Next(listePokemon.Count)];
+                    int pvMaxRencontre = pokemonRencontre.PointsDeVie;
+                    int pvMaxJoueur = pokemonJoueur.PointsDeVie;
+
+                    Animation.Fight_Anim();
+                    Console.WriteLine("\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
+                    TextDisplay.Sleeping($"\nVotre Pokémon actuel : {pokemonJoueur.Nom}\n", 10);
+                    TextDisplay.Sleeping($"{pokemonJoueur.Nom} - \nHP: {pokemonJoueur.PointsDeVie}\nType : {pokemonJoueur.Type}\nAttack: {pokemonJoueur.Attack}\nDefense: {pokemonJoueur.Defense}\nSpecial Attack: {pokemonJoueur.SpecialAttack}\nSpecial Defense: {pokemonJoueur.SpecialDefense}\nSpeed: {pokemonJoueur.Speed}", 10);
+                    //On attend que l'utilisateur appuie sur une touche pour continuer
+                    Console.ReadKey();
+                    fight_end = true;
+
+                    Console.Clear();
+                    // Combat
+                    while (fight_end)
+                    {
+                        if (pokemonJoueur.PointsDeVie <= 0)
+                        {
+                            Console.WriteLine($"Vous avez été vaincu par le {pokemonRencontre.Nom} sauvage !");
+                            Console.WriteLine();
+                            Console.WriteLine("votre Pokémon est soignée");
+                            pokemonJoueur.PointsDeVie = pvMaxJoueur;
+                            Thread.Sleep(500);
+                            gameover = true;
+
+                            fight_end = false;
+                            break;
+
+                        }
+                        if (pokemonRencontre.PointsDeVie <= 0)
+                        {
+                            Console.WriteLine($"Vous avez vaincu le {pokemonRencontre.Nom} sauvage !\n");
+                            Console.WriteLine("Votre Pokémon est soigné\n");
+                            pokemonJoueur.PointsDeVie = pvMaxJoueur;
+                            Console.ReadKey();
+
+                            fight_end = false;
+
+                            break;
+
+                        }
+                        Console.Clear();
+                        Console.WriteLine("Choisi une option :");
+                        Console.WriteLine("╔══════════════════════════════════╗");
+                        Console.WriteLine("║               CHOIX              ║");
+                        Console.WriteLine("╠══════════════════════════════════╣");
+                        Console.WriteLine("║1.Utiliser une Capacité           ║");
+                        Console.WriteLine("║2.Fuir le Combat                  ║");
+                        Console.WriteLine("║3.Attraper un Pokéball            ║");
+                        Console.WriteLine("║4.Changer de Pokémon              ║");
+                        Console.WriteLine("╚══════════════════════════════════╝");
+                        int choice = DemanderChoixJoueur();
+                        switch (choice)
+                        {
+                            case 1:
+                                Console.WriteLine();
+                                Console.Clear();
+                                Console.WriteLine("╔══════════════════════════════════╗");
+                                Console.WriteLine("║               COMBAT             ║");
+                                Console.WriteLine("╠══════════════════════════════════╣");
+                                Console.WriteLine($"║                 {pokemonRencontre.Nom}: {pokemonRencontre.PointsDeVie}/{pvMaxRencontre} PV║");
+                                Console.WriteLine("║                                  ║");
+                                Console.WriteLine("║                                  ║");
+                                Console.WriteLine($"║ {pokemonJoueur.Nom}: {pokemonJoueur.PointsDeVie}/{pvMaxJoueur} PV                 ║");
+                                Console.WriteLine("╚══════════════════════════════════╝");
+
+
+
+
+                                // Tour du joueur
+                                Console.WriteLine("\nC'est à votre tour :");
+                                AfficherCapacitesJoueur(pokemonJoueur);
+                                int choixCapacite = DemanderChoixCapacite(pokemonJoueur.Capacites.Count);
+                                Move.ManageMoveJ(pokemonJoueur, pokemonRencontre, pokemonJoueur.Capacites[choixCapacite - 1]);
+
+
+                                // Tour du Pokémon sauvage
+                                Console.WriteLine($"\nC'est au tour du {pokemonRencontre.Nom} sauvage :");
+                                iaEasy(pokemonRencontre, pokemonJoueur);
+                                break;
+                            case 2:
+                                Console.WriteLine("Vous fuyez le combat");
+                                Thread.Sleep(500);
+                                fight_end = false;
+                                break;
+                            case 3:
+                                Console.WriteLine("Pokeball lancé !");
+                                Random catchPokemon = new Random();
+                                Thread.Sleep(500);
+
+                                if (catchPokemon.Next(1, 10) < 1)
+                                {
+                                    Console.WriteLine($"Vous avez capturé {pokemonRencontre.Nom} !");
+                                    PokeTeam.AjouterPokemon(pokemonRencontre);
+                                    Console.WriteLine($"Voici le pokémon capturé !");
+                                    Console.WriteLine($"{pokemonRencontre.Nom} - \nHP: {pokemonRencontre.PointsDeVie}\nType : {pokemonRencontre.Type}\nAttack: {pokemonRencontre.Attack}\nDefense: {pokemonRencontre.Defense}\nSpecial Attack: {pokemonRencontre.SpecialAttack}\nSpecial Defense: {pokemonRencontre.SpecialDefense}\nSpeed: {pokemonRencontre.Speed}");
+                                    Thread.Sleep(3000);
+                                    fight_end = false;
+
+
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Le pokémon s'est échapée");
+                                    Console.WriteLine();
+
+                                    Console.WriteLine($"\nC'est au tour du {pokemonRencontre.Nom} sauvage :");
+                                    iaEasy(pokemonRencontre, pokemonJoueur);
+                                }
+                                break;
+                            case 4:
+                                Console.WriteLine();
+                                break;
+                        }
+
+
+
+
+                    }
+
+                    // Réinitialiser la case de la carte à 'H' seulement si le Pokémon n'a pas été vaincu
+                    carte[playerPosY, playerPosX] = 'H';
+                    if (gameover)
+                    {
+
+                        Story.GameOver();
+                    }
+                    Sound.AutoOST();
+        }
+            
+        
 
 
 
